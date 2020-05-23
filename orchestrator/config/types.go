8b99ea7e7915f7ca660a8279c1c9888629f3ca55
SayedAlesawy <sayedalesawy14@gmail.com>
2020-05-23 18:32:49 +0200

    reduce monitor cpu usage

--- orchestrator/config/types.go
+++ orchestrator/config/types.go
@@ -7,7 +7,9 @@ type ConfigurationManager struct {
 
 // HealthCheckMonitorConfig Houses the configurations of the healthcheck monitor
 type HealthCheckMonitorConfig struct {
-	IP            string `yaml:"ip"`             //IP of the monitor
-	Port          string `yaml:"port"`           //Port on which it receives healthchecks
-	LivenessProbe int    `yaml:"liveness_probe"` //A duration (in secs) after which a process is considered dead
+	IP                       string `yaml:"ip"`                         //IP of the monitor
+	Port                     string `yaml:"port"`                       //Port on which it receives healthchecks
+	LivenessProbe            int    `yaml:"liveness_probe"`             //A duration (in secs) after which a process is considered dead
+	HealthCheckInterval      int    `yaml:"health_check_interval"`      //The frequency of polling for health checks
+	LivenessTrackingInterval int    `yaml:"liveness_tracking_interval"` //The frequency for checking dead processes
 }
