8fd946078773b2e143d6a2486f273b2795c6d1a2
kareem <kareememad400@gmail.com>
2020-05-28 11:48:14 +0200

    params parsing for execution

--- executor/main.py
+++ executor/main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import logging
 import atexit
+from params_parser import parse_process_args
 from heartbeat import HeartBeat
 
 
@@ -12,18 +13,24 @@ stream = logging.StreamHandler(sys.stdout)
 stream.setLevel(logging.INFO)
 logger.addHandler(stream)
 
+
 if __name__ == "__main__":
+
+    process_args = parse_process_args()
+    if process_args.get('error'):
+        exit()
 
     def exit_handler():
         if heartbeat:
             heartbeat.gracefull_shutdown = True
-        print('[EXEC] Process Tear Down')
+        logger.info('[EXEC] Process shutdown successfully')
     atexit.register(exit_handler)
 
     logger.info(f'[EXEC]  Model executor process started with id-{os.getpid()}')
     heartbeat = HeartBeat(process_id=os.getpid())
     heartbeat.daemon = True
     heartbeat.start()
+
     logger.info('[EXEC] waiting for heartbeat to terimnate')
     heartbeat.join()
     logger.info('[EXEC] heartbeat terminated, main process going down')
---
+++ executor/params_parser.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+logger = logging.getLogger()
+
+tag = '[executor-parser]'
+
+
+def validate_args(args):
+    if not os.path.isfile(args.model_path) or not os.path.isfile(args.video_path) or not os.path.isfile(
+            args.model_config_path):
+        logger.exception(f'{tag} File Paths passed are not accessible {args.model_path} | {args.video_path} | {args.model_config_path}') # noqa
+        return False
+    return True
+
+
+def parse_process_args():
+    parser = argparse.ArgumentParser(description='Read Execution config')
+    parser.add_argument('--video-path', help='target video path on disk', required=True)
+    parser.add_argument('--model-path', help='model.h5 file path to load model weights', required=True)
+    parser.add_argument('--model-config-path',
+                        help='contains info for how the model input is expected',
+                        required=True)
+    args = parser.parse_args()
+
+    if validate_args(args):
+        return {'process_args': args}
+    else:
+        return {'error': 'Failed to parse args'}
