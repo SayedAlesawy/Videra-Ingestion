19d5b63007ca5b44c162c718567dd8e9d9c88661
kareem <kareememad400@gmail.com>
2020-05-28 12:16:05 +0200

    hide exit task in params_parser

--- executor/params_parser.py
+++ executor/params_parser.py
@@ -1,6 +1,6 @@
 import argparse
-import os
 import logging
+from os import path
 
 logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger()
@@ -9,23 +9,25 @@ tag = '[executor-parser]'
 
 
 def validate_args(args):
-    if not os.path.isfile(args.model_path) or not os.path.isfile(args.video_path) or not os.path.isfile(
-            args.model_config_path):
-        logger.exception(f'{tag} File Paths passed are not accessible {args.model_path} | {args.video_path} | {args.model_config_path}') # noqa
-        return False
+    files = {'model_path': args.model_path, 'video_path': args.video_path, 'config_path': args.model_config_path}
+    for argname in files:
+        fpath = files[argname]
+        if not path.isfile(fpath):
+            logger.exception(f'{tag} File Path for arg {argname} passed is not accessible {fpath}') # noqa
+            return False
     return True
 
 
 def parse_process_args():
     parser = argparse.ArgumentParser(description='Read Execution config')
-    parser.add_argument('--video-path', help='target video path on disk', required=True)
-    parser.add_argument('--model-path', help='model.h5 file path to load model weights', required=True)
-    parser.add_argument('--model-config-path',
+    parser.add_argument('-video-path', help='target video path on disk', required=True)
+    parser.add_argument('-model-path', help='model.h5 file path to load model weights', required=True)
+    parser.add_argument('-model-config-path',
                         help='contains info for how the model input is expected',
                         required=True)
     args = parser.parse_args()
 
     if validate_args(args):
-        return {'process_args': args}
+        return args
     else:
-        return {'error': 'Failed to parse args'}
+        exit()
