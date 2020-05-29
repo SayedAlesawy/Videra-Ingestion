b3526ba77dad90151ebde5b7e3c9617c85e30ef9
Yasmeen Ahmed <yeasmeenahmed@gmail.com>
2020-05-29 21:14:21 +0200

    merged with main

--- executor/main.py
+++ executor/main.py
@@ -1,10 +1,9 @@
 import os
 import sys
 import logging
-import time
-from heartbeat import HeartBeat
+import atexit
 from params_parser import parse_process_args
-# from execution_worker import ExecutionWorker
+from heartbeat import HeartBeat
 from receiver import Receiver
 
 BUSYFLAG = 0
@@ -21,23 +20,29 @@ stream = logging.StreamHandler(sys.stdout)
 stream.setLevel(logging.INFO)
 logger.addHandler(stream)
 
+
 if __name__ == "__main__":
-    logger.info(f'Model executor process started with id-{os.getpid()}')
-    heartbeat = HeartBeat()
-    heartbeat.daemon = False
+
+    process_args = parse_process_args()
+
+    def exit_handler():
+        if heartbeat:
+            heartbeat.gracefull_shutdown = True
+        logger.info('[EXEC] Process shutdown successfully')
+    atexit.register(exit_handler)
+
+    logger.info(f'[EXEC]  Model executor process started with id-{os.getpid()}')
+    heartbeat = HeartBeat(process_id=os.getpid())
+    heartbeat.daemon = True
     heartbeat.start()
-    logger.info('waiting for heartbeat to terimnate')
-    time.sleep(6)
-    # heartbeat.gracefull_shutdown = True
 
-    args = parse_process_args()
+    logger.info('[EXEC] waiting for heartbeat to terimnate')
+    heartbeat.join()
+    logger.info('[EXEC] heartbeat terminated, main process going down')
+
     receiver = Receiver(videoPath=args.video_path, modelPath=args.model_path, modelConfigPath=args.model_config_path)
-
     while True:
         receiver.get_batch_metadata()
         BUSYFLAG = 1
         process()
         BUSYFLAG = 0
-
-    heartbeat.join()
-    logger.info('heartbeat terminated')
