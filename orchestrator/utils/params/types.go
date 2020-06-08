4afd28eea401a5b840966a76d8b817d27909c1a9
SayedAlesawy <sayedalesawy14@gmail.com>
2020-06-08 17:28:29 +0200

    add execution group id to orch params

--- orchestrator/utils/params/types.go
+++ orchestrator/utils/params/types.go
@@ -2,10 +2,11 @@ package params
 
 // OrchestratorParams Houses the command line params passed to the orchestrator
 type OrchestratorParams struct {
-	VideoPath       string                 //Path of the video chunk to be processed
-	ModelPath       string                 //Path of the user CV model to be applied
-	ModelConfigPath string                 //Path of the user CV model config to be applied
-	StartIdx        int64                  //Start index from which processing would start
-	FrameCount      int64                  //Frame count to be processes
-	ArgsMap         map[string]interface{} //Houses all params in a key-value format
+	VideoPath        string                 //Path of the video chunk to be processed
+	ModelPath        string                 //Path of the user CV model to be applied
+	ModelConfigPath  string                 //Path of the user CV model config to be applied
+	ExecutionGroupID string                 //Unique ID for the execution group
+	StartIdx         int64                  //Start index from which processing would start
+	FrameCount       int64                  //Frame count to be processes
+	ArgsMap          map[string]interface{} //Houses all params in a key-value format
 }
