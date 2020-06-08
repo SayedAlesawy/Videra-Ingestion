4a3549dfe1ac9d126a02281f5fd95fa3accb0486
SayedAlesawy <sayedalesawy14@gmail.com>
2020-06-08 17:21:30 +0200

    edit ingestion manager config

--- orchestrator/config/ingestion_manager.go
+++ orchestrator/config/ingestion_manager.go
@@ -2,10 +2,15 @@ package config
 
 // IngestionManagerConfig Houses the configurations of the ingestion manager
 type IngestionManagerConfig struct {
-	WorkerPoolIP            string `yaml:"worker_pool_ip"`           //IP of the worker pool
-	WorkersScanningInterval int    `yaml:"worker_scanning_interval"` //The frequency at which the manager checks for non-busy workers
-	JobSize                 int64  `yaml:"job_size"`                 //Frame count per job
-	JobSendTimeout          int    `yaml:"job_send_timeout"`         //Timeout for sending job to worker pool
+	JobSize int64 `yaml:"job_size"` //Frame count per job
+	Queues  Queue `yaml:"queues"`   //Defines queues names used in the queueing system
+}
+
+// Queue Defines a queue used in the ingestion manager
+type Queue struct {
+	Todo       string `yaml:"todo"`        //Queue for to be done jobs
+	InProgress string `yaml:"in_progress"` //Queue for currently executing jobs
+	Done       string `yaml:"done"`        //Queue for already executed jobs
 }
 
 // IngestionManagerConfig A function to return the ingestion manager config
