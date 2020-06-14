09ef94c9d16cfcf52f932d23795d49efb69a4182
Yasmeen Ahmed <yeasmeenahmed@gmail.com>
2020-06-14 18:41:42 +0200

    added logging, removed all prints, added output path

--- merger/merg.py
+++ merger/merg.py
@@ -1,40 +1,43 @@
 import json
-from os import listdir
-from os.path import isfile, join
-import argparse
+import logging
+from os import listdir, stat, mkdir
+from os.path import isfile, join, dirname
+from params_parser import parse_process_args
 
-def get_ranges(message):  
-    encoded_message = []
+logging.getLogger().setLevel(logging.INFO)
+logger = logging.getLogger()
+tag = '[Merger]'
+
+
+def get_ranges(run_count):
+    run = []
     i = 0
-   
-    while (i <= len(message)-1): 
+
+    while (i <= len(run_count)-1):
         count = 1
-        ch = message[i] 
-        j = i 
-        while (j < len(message)-1): 
-            if (message[j] == message[j+1]): 
+        ch = run_count[i]
+        j = i
+        while (j < len(run_count)-1):
+            if (run_count[j] == run_count[j+1]):
                 count = count+1
                 j = j+1
-            else: 
+            else:
                 break
-        encoded_message.append([ ch , count])
+        run.append([ch, count])
         i = j+1
-    return(encoded_message)
-
+    return(run)
 
 
 def process_file(file_path):
     res = {}
+
     with open(file_path,) as f:
 
         data = json.load(f)
-        # print(data)
         keys = list(data.values())
-        
+
         ranges = get_ranges(keys)
         start_idx = 0
-
-        # print(ranges)
 
         for i, pair in enumerate(ranges):
 
@@ -51,67 +54,60 @@ def process_file(file_path):
 
             first = data_keys[first]
             second = data_keys[second]
-            
+
             tmp = str(first) + "-" + str(second)
 
-            # print(tmp, start_idx)
             start_idx += pair[1]
             res.update({tmp: data.get(first)})
 
-        # print("res: ", res)
     return res
 
 
-def write_file(file_name,res):
-    with open('./' + file_name, 'w') as f:
+def write_file(file_path, res):
+    directory = dirname(file_path)
+    try:
+        stat(directory)
+    except(Exception):
+        mkdir(directory)
+
+    with open('./' + file_path, 'w') as f:
         json.dump(res, f)
+
 
 def merg_boundry(total_res, res):
     last_idx = list(res.keys())[-1]
-    last_value  = res.get(last_idx)
+    last_value = res.get(last_idx)
 
     res_idxs = list(total_res.keys())
     res_last_idx = res_idxs[-1]
     res_last_value = total_res.get(res_last_idx)
 
-    print("in fun: ", last_value, res_last_value)
-    print("in fun: ", res)
-    print("in fun: ", total_res)
     if(last_value == res_last_value):
         start = res_last_idx.split("-")[0]
         end = last_idx.split("-")[1]
         del res[last_idx]
         del total_res[res_last_idx]
-        print("!!!!total: ", total_res)
-        print("!!!!!res:", res)
+
         res.update({last_value: start + "-" + end})
-        
 
     total_res.update(res)
-    print("before ret:", total_res)
     return total_res
 
-def parse_process_args():
-
-    parser = argparse.ArgumentParser(description='Read Execution config')
-    parser.add_argument('-files-path', required=True)
-    parser.add_argument('-no-files', required=True)
-    args = parser.parse_args()
-
-    return args.files_path, int(args.no_files)
-
-    
 
 if __name__ == "__main__":
-    mypath , number_of_files= parse_process_args()
 
-    # mypath ="../orchestrator/output"
-    files_list = [f for f in listdir(mypath) if isfile(join(mypath, f)) and ".json" in f]
+    args = parse_process_args()
+    mypath = args.input_files_path
+    number_of_files = args.no_files
+    output_path = args.output_file_path
 
-    files_list.sort(key= lambda x: int(x.split("-")[1]))
+    files_list = [f for f in listdir(mypath)
+                  if isfile(join(mypath, f)) and ".json" in f]
 
+    files_list.sort(key=lambda x: int(x.split("-")[1]))
 
     total_res = {}
+    actual_number_of_files = len(files_list)
     for i, curr_file in enumerate(files_list):
 
         path = join(mypath, curr_file)
@@ -121,10 +117,13 @@ if __name__ == "__main__":
             total_res.update(res)
             continue
 
-        total_res =  merg_boundry(total_res, res)
+        total_res = merg_boundry(total_res, res)
 
         total_res.update(res)
+        logger.info(f'{tag} finished processing {i+1} files out of\
+                    {actual_number_of_files}')
         if i == number_of_files:
             break
 
-    write_file("output.json",total_res)
+    output_path = join(output_path, curr_file.split(".")[0] + ".json")
+    write_file(output_path, total_res)
